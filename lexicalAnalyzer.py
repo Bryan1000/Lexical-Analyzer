e048727467e0965a74a959fd0c872e37cab3d7f1
Bryan1000 <brivas100@hotmail.com>
2024-03-29 15:57:35 -0700

    Updated regex function

--- lexicalAnalyzer.py
+++ lexicalAnalyzer.py
@@ -8,10 +8,10 @@ def tokenize():
     keyword_regex = r'\b(?:' + '|'.join(allKeywords) + r')\b'
 
     #regex for the operators, separatos, identiers, and integers (tokenize)
-    operator_regex = r'[\+\-\*/=<>!]=?'
+    operator_regex = r'[\+\-\*/%=<>&|^~]=?|<<=?|>>=?|\*\*=?|\/\/=?|==|!=|<=|>=|in|not in|is|is not|and|or|not'
     separator_regex = r'[()\{\};]'
     identifier_regex = r'[a-zA-Z_]\w*'
-    integer_regex = r'\b\d+\b'
+    integer_regex = r'\b\d+(\.\d+)?\b'
 
     # Combine regular expressions 
     tokens = '|'.join([keyword_regex, operator_regex, separator_regex, identifier_regex, integer_regex])
