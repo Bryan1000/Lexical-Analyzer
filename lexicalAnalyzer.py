10fa8a36b82c51e7f44ece40e35f01b256328fb9
Bryan1000 <brivas100@hotmail.com>
2024-03-31 23:05:09 -0700

    Updated analyzer for c++ and new test case (for strings)

--- lexicalAnalyzer.py
+++ lexicalAnalyzer.py
@@ -1,17 +1,14 @@
 import re
-import keyword
-
-# Get all keywords
-allKeywords = keyword.kwlist
 
 # Regular expression to match any of the keywords
-keyword_regex = r'\b(?:' + '|'.join(allKeywords) + r')\b'
+keyword_regex = r'\b(?:auto|break|alignas|alignof|and|and_eq|asm|atomic_cancel|atomic_commit|atomic_noexcept|char8_t|char16_t|char32_t|compl|concept|consteval|constexpr|constinit|const_cast|co_await|nullptr|bitand|bitor|bool|ostream|std|str|assert_eq||ifstream|char|cout|cin|try|catch|include|stoi|get|stod|case|catch|class|const|continue|default|dynamic_cast|trunc|thread_local|sycronized|delete|do|double|else|enum|module|defined|explicit|export|extern|false|float|for|friend|goto|if|inline|int|long|mutable|namespace|new|operator|private|protected|public|register|reinterpret_cast|return|short|signed|sizeof|static|static_assert|static_cast|struct|switch|template|this|throw|true|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|xor|xor_eq|not|noexcept|or|while)\b'
 
 # Regular expressions for the operators, separators, identifiers, and integers (tokens)
-operator_regex = r'[-+*/%=<>&|^~]=?|<<=?|>>=?|\*\*=?|\/\/=?|==|!=|<=|>=|in|not in|is|is not|and|or|not|\b(?:[+\-*/%=<>&|^~]|<<|>>|\*\*|\/\/|==|!=|<=|>=|in|not in|is|is not|and|or|not)\b'
-separator_regex = r'[()\[\]{};.:]'
+operator_regex = operator_regex = r'[-+*/%=<>&|^~!]=?|<<=?|>>=?|\*\*=?|\/\/=?|==|!=|<=|>=|&&|\|\||::|\+\+|--|->|\.|->\*|\[\]|&=|\|=|\^=|<<=|>>=|~|&|\||\^|\(|\)|\[|\]|{|}|;|,|:|\?|\.|\.\*|->|new|delete|new\[\]|delete\[\]|"'
+
+separator_regex = r'[()\[\]{};,.:]'
 identifier_regex = r'[a-zA-Z_]\w*'
-integer_regex = r'\b\d+\b'
+integer_regex = r'\b(?:0[xX][0-9a-fA-F]+|0[0-7]*|\d+)\b'
 
 # Combine regular expressions 
 tokens = '|'.join([keyword_regex, operator_regex, separator_regex, identifier_regex, integer_regex])
@@ -41,25 +38,39 @@ def main():
     # Remove empty strings from the list of tokens
     tokens = [token for token in tokens if token]
     
-    tokens_classified = {}
+    tokens_classified = []
+    is_string = False
+    temp_str = ""
     # Append token classification to list
-    for token in tokens:
-        if re.match(operator_regex, token):
+    for i in range(len(tokens)):
+        if is_string and tokens[i] != '"':
+            temp_str = temp_str + " " + tokens[i]
+            continue
+            
+        if tokens[i] == '"':
+            if is_string:
+                tokens_classified.append([temp_str.strip(), 'string literal'])
+            is_string = not is_string
+            tokens_classified.append([tokens[i], 'operator'])
+        elif re.match(separator_regex, tokens[i]):
+            tokens_classified.append([tokens[i],'separator'])
+        elif re.match(operator_regex, tokens[i]):
             # Split compound operators into individual tokens
-            ops = re.findall(r'\b\w{2,}\b|.', token)
-            tokens_classified.update({op: 'operator' for op in ops if op.strip()})
-        elif re.match(separator_regex, token):
-            tokens_classified[token] = 'separator'
-        elif re.match(keyword_regex, token):
-            tokens_classified[token] = 'keyword'
-        elif re.match(integer_regex, token):
-            tokens_classified[token] = 'integer'
+            ops = re.findall(r'\b\w{2,}\b|.', tokens[i])
+            for op in ops:
+                tokens_classified.append([op, 'operator'])
+        elif re.match(keyword_regex, tokens[i]):
+            tokens_classified.append([tokens[i],'keyword'])
+        elif re.match(integer_regex, tokens[i]):
+            tokens_classified.append([tokens[i], 'integer'])
+        elif tokens[i-1] == '"' and tokens[i+1] == '"':
+            tokens_classified.append([tokens[i],'string literal'])
         else:
-            tokens_classified[token] = 'identifier'
+            tokens_classified.append([tokens[i], 'identifier'])
     
     # Printing output as described in instructions
-    for token, classification in tokens_classified.items():
-        print(f'"{token}" = {classification}')
+    for tokens, classification in tokens_classified:
+        print("\"" + tokens +"\" = " + classification)
 
 
 if __name__ == "__main__":
